c4f2417ed2225e473e2b0d1c41447c30135f533a
tetrix0305 <sotrix0305@hotmail.com>
2019-04-23 13:13:04 +0200

    hoy

---
+++ JS/09_operadores.js
@@ -0,0 +1,38 @@
+// Asignacion
+
+let x = 23
+// x = x + 1
+x ++
+// x = x -1
+x--
+
+// Aritmeticos
+let y = 2
+
+let r
+r = x + y // 25
+r = x - y // 21
+r = x * y // 46
+r = x / y // 11.5
+r = parseInt ( x / y) // 11
+// r= Nath.round ()
+// r= Nath.floor ()
+// r= Nath.ceil ()
+r = x % y
+console.log(r)
+
+let y - 2
+y += 3
+// y - y +3
+y = 3
+// y = y -3
+y *= 2
+y /- 3
+// y = y % 2
+y %= 2
+
+
+// operador temario
+let x = 15
+if (x)- 18
+
