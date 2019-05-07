7dd77469f7e2187e312648f4b16bdf7d858161ca
Sonia <sotrix0305@hotmail.com>
2019-05-08 00:05:09 +0200

    hoy

--- JS/30_pali.js
+++ JS/30_pali.js
@@ -1,22 +1,55 @@
 /**
- * Comprobar si una frase es un palindromo
- * Se le 
+ * Crear una función que nos dice si una frase es un palíndromo o no,
+ * es decir, si se lee igual en ambos sentidos
+ * La ruta nos aportó otro paso natural
+ * Atar a la rata...
+ * 
  */
 
+ /**
+ * function isPalindromo
+ * @param: cadena: string
+ * @returns: boolean
+ * } cadena 
+  */
+
+ function isPali(cadena = '') {
+    let r = true
+    let cadenaSinEspacios = cadena.split(' ').join('').toLowerCase() // split de palabras
+    // console.log(cadenaSinEspacios)
+    let cadenaInversa = cadenaSinEspacios.split('').reverse().join('') // split de letras
+    // console.log(cadenaInversa)
+    if (cadenaSinEspacios !== cadenaInversa) {
+        r = false
+    }
+    return r
+}
 
 
+/* function isPali2(cadena = '') {
+    let r = true
+    // let cadenaSinEspacios = cadena.split(' ').join('')
+    // console.log(cadenaSinEspacios)
+    let cadenaInversa = cadenaSinEspacios.split('').reverse().join('')
+    // console.log(cadenaInversa)
+    let cadenaInversa = cadena.split('').reverse().join('')
+    if (cadena.toLowerCase() !== cadenaInversa) r = false // MALA PRÁCTICA
+    return r
+} */
 
+function isPaliShort(cadena = '') {
+    return cadena.toLowerCase() === cadena.split(' ').split('').reverse().join('') ? true: false
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+(function () {
+    let textos = [
+        'Esto no es un palíndromo',
+        'La ruta nos aporto otro paso natural',
+        'Atar a la rata'
+    ]
+    for (let i = 0; i < textos.length; i++) {
+        const texto = textos[i];
+        console.log(texto)
+        console.log(isPali(texto))
+    }
+})()
--- JS/31_mas_arrays.js
+++ JS/31_mas_arrays.js
@@ -1,22 +1,31 @@
-let nombres = ['Pepe', 'Juan', 'Bernardette','Penelope','Eustaquio']
+let nombres = ['Pepe', 'Juan', 'Bernardette', 'Penelope', 'Eustaquio']
 
-//Slice es una funcion inmutable
-console.log(nombres.slice(3,4))
+// Slice es una función que no modifica el array original (inmutable)
+console.log(nombres.slice(0,3)) // Incluye la posición inicial, pero no la posición final.
 
-//Splice es una funcion mutable
+// Splice es una función mutable, es decir, el array original queda afectado por los cambios que realicemos
 
-let newNombres = nombres.splice(1,3)
+let newNombres = nombres.splice(1,1) // Splice n,n lo que hace es quitar un elemento del array
 console.clear()
 console.log(nombres)
 console.log(newNombres)
 
-//Colas y pilas - todas 
 
-nombres.push() // AÑADE AL FINAL
-nombres.pop() // QUITA AL FINAL 
+// Colas y pilas - todas mutables
 
-nombres.shift() // QUITA EL PRIMERO
-nombres.unshift() // AÑADIR AL PRINCIPIO
+// Pilas 
+nombres.push() // Añade al final - Esta es la más utilizada
+nombres.pop() // Quita al final
 
-nombres.push('Aurora')
+//Colas
+nombres.shift() // Quitar el primero
+nombres.unshift() // Añadir al principio
 
+// nombres.push('Aurora')
+// nombres[nombres.length] = 'Aurora'
+
+
+let datos = [1,70,7,5,9]
+let masDatos = [1,4,6]
+datos.push(masDatos) // Esto conseguiría hacer un array de arrays
+console.log(datos)
