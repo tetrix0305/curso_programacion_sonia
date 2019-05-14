244ef7a8d51b7c31dce1eb832756de59ee4af03b
Sonia <sotrix0305@hotmail.com>
2019-05-14 23:40:24 +0200

    HOY

--- JS/JS_web/05_interactivo_ES6/js/app.js
+++ JS/JS_web/05_interactivo_ES6/js/app.js
@@ -1,3 +1,3 @@
-import { Saludador } from "./saludador.js"   
+import { Saludador } from './saludador.js'
 
-document.addEventListener ('DOMContentLoaded',()=> console.log(new Saludador()))
+document.addEventListener('DOMContentLoaded', () => console.log(new Saludador()) )
--- JS/JS_web/05_interactivo_ES6/js/saludador.js
+++ JS/JS_web/05_interactivo_ES6/js/saludador.js
@@ -1,43 +1,39 @@
 export class Saludador {
-    constructor(){
-        this.aQuien=''
+    constructor() {
+        this.aQuien = ''
         this.msg = [`Hola `, `Adios `]
-          
-    /**Seleccionar nodos del DOM */
-    this.inNombre = document.querySelector('#in-nombre')
-    this.btnSaludar = document.querySelector('#btn-saludar')
-    this.btnDespedirse = document.querySelector('#btn-despedirse')
-    this.btnBorrar = document.querySelector('#btn-borrar')
-    this.output = document.querySelector('#output')
 
- /**Para asignar manejadores a los nodos */
-    this.btnSaludar.addEventListener('click',this.saludar.bind(this))
-    this.btnDespedirse.addEventListener('click', this.despedirse.bind(this))
-    this.btnBorrar.addEventListener('click', this.borrar.bind(this))
+        /** Seleccionar nodos del DOM */
+        this.inNombre = document.querySelector('#in-nombre')
+        this.btnSaludar = document.querySelector('#btn-saludar')
+        this.btnDespedirse = document.querySelector('#btn-despedirse')
+        this.btnBorrar = document.querySelector('#btn-borrar')
+        this.output = document.querySelector('#output')
+        
+        /**Asignar manejadores a los nodos */
+        this.btnSaludar.addEventListener('click', this.saludar.bind(this))
+        this.btnDespedirse.addEventListener('click', this.despedirse.bind(this))
+        this.btnBorrar.addEventListener('click', this.borrar.bind(this))
     }
 
     saludar() {
         if (this.inNombre.value) {
-        this.aQuien = '<b>'+ this.inNombre.value + '</b>'
-        // console.log(this.msg[0] + this.aQuien)
-        this.output.innerHTML = this.msg[0] + this.aQuien
+            this.aQuien = '<b>' + this.inNombre.value + '</b>'
+            // console.log(this.msg[0] + this.aQuien)
+            this.output.innerHTML = this.msg[0] + this.aQuien
         }
-       
-            
     }
 
     despedirse() {
         if (this.inNombre.value) {
-        this.aQuien ='<b>' + this.inNombre.value + '</b>'
-        //console.log(this.msg[1] + this.aQuien)
-        this.output.innerHTML = this.msg[1] + this.aQuien
+            this.aQuien = '<b>' + this.inNombre.value + '</b>'
+            // console.log(this.msg[1] + this.aQuien)
+            this.output.innerHTML = this.msg[1] + this.aQuien            
+        }
     }
-}
 
     borrar() {
         this.output.innerHTML = ''
-        this.inNombre.value =''
+        this.inNombre.value = ''
     }
 }
-        
-
