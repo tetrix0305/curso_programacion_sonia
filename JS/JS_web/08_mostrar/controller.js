73f3815928d9869b69151ea77a1624c2deccbfb8
tetrix0305 <sortrix0305@hotmail.com>
2019-05-15 13:23:45 +0200

    hoy

--- JS/JS_web/08_mostrar/controller.js
+++ JS/JS_web/08_mostrar/controller.js
@@ -1,29 +1,39 @@
 /** como es una funcion no puede faltar los parentesis y corchetes
  Es un fichero que exporta algo*/  
  
-export function controller() {
+ export function controller() {
 
     console.log('Controller cargado')
 
-    let nombre = ''
-    let msgInicial
+    /** Declarar variables */
+    let isVisibleDiv = false
+    let msg = [' [....monstras menos]',]
 
-    /**Nodos del DOM  */
-    const inNombre    = document.querySelector('#in-nombre')
-    const btnborrar   = document.querySelector('#btn-borrar')
-    const output      = document.querySelector('#output')
-
-    msgInicial = output.innerHTML + ' '
-
-    /** Asignar manejadores de eventos*/
-    inNombre.addEventListener('input',onInputNombre)
-    btnborrar.addEventListener('click', borrar)
-
-    function onInputNombre () {
-        nombre = inNombre.value
-        output.innerHTML  = msgInicial + nombre.toUpperCase()
+    /** Modos del DOM */
+    let nodos = {
+        btnCambiar: document.querySelector('#btn-cambiar'),
+        divHide: document.querySelector ('#div-hide')
     }
-}
-    function borrar() {
-        
+    msg.push (nodos.btnCambiar.innerHTML)
+
+       /**Asignar manejadores de eventos */
+    nodos.btnCambiar.addEventListener('click', cambiar )
+    
+    function cambiar (){
+        if (isVisibleDiv) {
+            // hay que ocultar
+            modos.divHide.classList.
+            console.log(nodos.divHide.classList.value)
+            // ya hemos ocultado
+            nodos.btnCambiar.innerHTML = msg [1]
+            isVisibleDiv = false
+        } else {
+            // hay que mostrar
+            nodos.divHide.classList.remove('hide')
+            console.log(nodos.divHide.classList.value)
+            // ya hemos mostrado
+            nodos.btnCambiar.innerHTML = msg [0]
+            isVisibleDiv = true
+        }
+        isVisibleDiv = !isVisibleDiv
     }
