59e606a9f2fc8caa7f33c63bedbb79e33923c9ac
tetrix0305 <sortrix0305@hotmail.com>
2019-06-04 13:07:41 +0200

    hoy

---
+++ JS_web/15-animales/js/bootstrap.js
@@ -0,0 +1,11 @@
+import {controller} from './controller.js'
+ 
+/*
+Es buena practica crear un módulo aparte e importar la función principal
+*/
+
+// Proceso ASINCRONO
+document.addEventListener('DOMContentLoaded', controller)
+
+
+
