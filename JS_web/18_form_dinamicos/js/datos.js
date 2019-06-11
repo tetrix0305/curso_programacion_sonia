01ef46d3e22866f625875c6b139f42cb008dd564
tetrix0305 <sortrix0305@hotmail.com>
2019-06-11 12:27:37 +0200

    Ajax mejorado

--- JS_web/18_form_dinamicos/js/datos.js
+++ JS_web/18_form_dinamicos/js/datos.js
@@ -1,3 +1,23 @@
+Skip to content
+Why GitHub? 
+Enterprise
+Explore 
+Marketplace
+Pricing 
+Search
+
+Sign in
+Sign up
+1 2 4 alce65/front_cice
+ Code  Issues 0  Pull requests 0  Projects 0  Security  Insights
+Join GitHub today
+GitHub is home to over 36 million developers working together to host and review code, manage projects, and build software together.
+
+front_cice/JS_Web/18_form_dinamic/js/datos.js
+@alce65 alce65 Form dinamic final
+850cbe3 3 hours ago
+20 lines (19 sloc)  854 Bytes
+    
 export const GENEROS = [
     {value:'', label: ''},
     {value:'P', label: 'Policiaco', autores : [
@@ -16,3 +36,6 @@ export const GENEROS = [
         {value:'Shelly,Mary', label: 'Mary Shelley'},
     ]}
 ]
+
+// https://www.googleapis.com/books/v1/volumes?q=inauthor:${xxxxxx}&fields=items(volumeInfo(publisher,title,language))
+
