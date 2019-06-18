1d176621b3fdb2fc4f65e6cc0e4b1976aa2ab290
soniandrea <soni291100@gmail.com>
2019-06-18 13:22:56 +0200

    TypeScript

---
+++ TS/01_hola_mundo.js
@@ -0,0 +1,6 @@
+{
+let cadena:string 
+cadena ='Hola Mundo'
+cadena= 23
+console.log(cadena)
+}/*  */
---
+++ TS/02_tipos.js
@@ -0,0 +1,11 @@
+let nombre: string; //variable tipo nombre
+let edad: number; // variable tipo number
+let isAlumno: boolean; // variable tipo boolean
+
+nombre ='Pepe';
+edad = 23
+isAlumno = true;
+
+let profesion = 'Programador' // variable tipo string
+
+// let
